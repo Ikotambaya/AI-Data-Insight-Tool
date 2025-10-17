16caf54eb8117ee5ec752d00aa4e1f8fa0863298
Ikotambaya <ikotambaya1@gmail.com>
2025-10-17 10:43:00 +0100

    Create access_control.py

---
+++ access_control.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def init_access_control():
+    """Stub for access-control init."""
+    pass
+
+def access_control_page():
+    """Stub access-control page."""
+    st.info("Access-control module loaded (stub).")
