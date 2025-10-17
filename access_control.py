bd0b3069b0024daa8a02b7aa7f8473bcb8cace40
Ikotambaya <ikotambaya1@gmail.com>
2025-10-17 14:33:36 +0100

    Delete access_control.py

--- access_control.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def init_access_control():
-    """Stub for access-control init."""
-    pass
-
-def access_control_page():
-    """Stub access-control page."""
-    st.info("Access-control module loaded (stub).")
